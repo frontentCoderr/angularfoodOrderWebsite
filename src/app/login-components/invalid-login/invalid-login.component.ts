e8f09243c48301feac57bf75659fb6724ef5ed3a
frontentCoderr <104434992+frontentCoderr@users.noreply.github.com>
2022-04-27 17:46:33 +0530

    Add files via upload

---
+++ src/app/login-components/invalid-login/invalid-login.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-invalid-login',
+  templateUrl: './invalid-login.component.html',
+  styleUrls: ['./invalid-login.component.css']
+})
+export class InvalidLoginComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
