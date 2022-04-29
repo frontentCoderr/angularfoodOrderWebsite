3a4db4d98244c3f40b56d2861031e9e62ee04a08
frontentCoderr <104434992+frontentCoderr@users.noreply.github.com>
2022-04-29 13:28:02 +0530

    Update forgot-password.component.ts

--- src/app/login-components/forgot-password/forgot-password.component.ts
+++ src/app/login-components/forgot-password/forgot-password.component.ts
@@ -3,8 +3,7 @@ import { faLock } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-forgot-password',
-  templateUrl: './forgot-password.component.html',
-  styleUrls: ['./forgot-password.component.css']
+  templateUrl: './forgot-password.component.html'
 })
 export class ForgotPasswordComponent implements OnInit {
   faLock = faLock;
